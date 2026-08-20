e2d6d1053dafef05d04719910d8f168b35490dbb
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-08-21 02:30:11 +0530

    feat: use mobile hook, and shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,36 @@
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google"
-
+import type { Metadata } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'})
+export const metadata: Metadata = {
+  title: "Startup Rank",
+  description:
+    "The internet's live leaderboard where startups compete for the top spot.",
+  openGraph: {
+    title: "Startup Rank",
+    description:
+      "The internet's live leaderboard where startups compete for the top spot.",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Startup Rank",
+    description:
+      "The internet's live leaderboard where startups compete for the top spot.",
+  },
+}
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
 
 export default function RootLayout({
   children,
@@ -20,7 +41,12 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontSans.variable, "font-mono", jetbrainsMono.variable)}
+      className={cn(
+        "antialiased",
+        fontSans.variable,
+        "font-mono",
+        jetbrainsMono.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
